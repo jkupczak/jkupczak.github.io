ab47ea9891a5ad25cfe848c30ac12c74ba4729e6
James Kupczak <jimmykup@gmail.com>
2019-07-21 07:07:06 -0700

    Checklist and Carpool Calculator

---
+++ qa-checklist/init-firebase.js
@@ -0,0 +1,8 @@
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyDfiJ_ea_FQf0Y1Mx7JvZ7pAopzHs11oe8",
+  authDomain: "qa-checklist-ca246.firebaseapp.com",
+  projectId: "qa-checklist-ca246"
+});
+
+var db = firebase.firestore();
